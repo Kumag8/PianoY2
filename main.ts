ae1edde0d823446d5d156702a757f318d9d42f8c
Kumag8 <75565726+Kumag8@users.noreply.github.com>
2021-05-30 01:00:42 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P3) == 1) {
+    if (true) {
         music.ringTone(262)
-    } else if (pins.digitalReadPin(DigitalPin.P4) == 1) {
+    } else if (false) {
         music.ringTone(294)
-    } else if (pins.digitalReadPin(DigitalPin.P5) == 1) {
+    } else if (false) {
         music.ringTone(330)
-    } else if (pins.digitalReadPin(DigitalPin.P6) == 1) {
+    } else if (false) {
         music.ringTone(349)
-    } else if (pins.digitalReadPin(DigitalPin.P7) == 1) {
+    } else if (false) {
         music.ringTone(392)
-    } else if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+    } else if (false) {
         music.ringTone(440)
-    } else if (pins.digitalReadPin(DigitalPin.P9) == 1) {
+    } else if (pins.digitalReadPin(DigitalPin.P5) == 1 && pins.digitalReadPin(DigitalPin.P8) == 0 && pins.digitalReadPin(DigitalPin.P9) == 0 && pins.digitalReadPin(DigitalPin.P16) == 0) {
         music.ringTone(494)
-    } else if (pins.digitalReadPin(DigitalPin.P10) == 1) {
+    } else if (pins.digitalReadPin(DigitalPin.P5) == 1 && pins.digitalReadPin(DigitalPin.P8) == 0 && pins.digitalReadPin(DigitalPin.P9) == 0 && pins.digitalReadPin(DigitalPin.P16) == 0) {
         music.ringTone(523)
     } else {
         music.stopAllSounds()
