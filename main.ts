308f152095439148d0837b3ccb5532e11394652c
Kumag8 <75565726+Kumag8@users.noreply.github.com>
2021-05-30 01:35:44 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,14 @@
 basic.forever(function () {
-    if (true) {
+    if (pins.digitalReadPin(DigitalPin.P5) == 1) {
         music.ringTone(262)
-    } else if (false) {
+    } else if (pins.digitalReadPin(DigitalPin.P8) == 1) {
         music.ringTone(294)
-    } else if (false) {
+    } else if (pins.digitalReadPin(DigitalPin.P9) == 1) {
         music.ringTone(330)
-    } else if (false) {
+    } else if (pins.digitalReadPin(DigitalPin.P11) == 1) {
         music.ringTone(349)
-    } else if (false) {
+    } else if (pins.digitalReadPin(DigitalPin.P16) == 1) {
         music.ringTone(392)
-    } else if (false) {
-        music.ringTone(440)
-    } else if (pins.digitalReadPin(DigitalPin.P5) == 1 && pins.digitalReadPin(DigitalPin.P8) == 0 && pins.digitalReadPin(DigitalPin.P9) == 0 && pins.digitalReadPin(DigitalPin.P16) == 0) {
-        music.ringTone(494)
-    } else if (pins.digitalReadPin(DigitalPin.P5) == 1 && pins.digitalReadPin(DigitalPin.P8) == 0 && pins.digitalReadPin(DigitalPin.P9) == 0 && pins.digitalReadPin(DigitalPin.P16) == 0) {
-        music.ringTone(523)
     } else {
         music.stopAllSounds()
     }
